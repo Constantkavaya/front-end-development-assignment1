35751b5ae022472ac7813637ebcfe8f2de60b76b
Constantkavaya <kavayaconstant@gmail.comgit config --listgit config --global user.email kavayaconstant@gmail.com>
2021-03-09 18:09:41 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+function fruits(){
+    var fruits=["Avocado","Grapes","Mangoes","Oranges","Kiwi"];
+    for (let x= 0; x < fruits.length; x++) {
+        const element = fruits[x];
+        
+    console.log(fruits[x]);
+
+        
+    }    
+    
+}
+fruits();
